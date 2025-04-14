62e9444156ec0d6299ce5c4a6e46955b4736db77
Samuele-Andreoli01 <samueleandreoli01@gmail.com>
2025-04-14 23:05:37 +0100

    modified AddNoteFragment.java

--- app/src/main/java/uk/ac/hope/mcse/android/coursework/AddNoteFragment.java
+++ app/src/main/java/uk/ac/hope/mcse/android/coursework/AddNoteFragment.java
@@ -1,64 +1,44 @@
 package uk.ac.hope.mcse.android.coursework;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddNoteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import uk.ac.hope.mcse.android.coursework.model.Note;
+
 public class AddNoteFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddNoteFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddNoteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddNoteFragment newInstance(String param1, String param2) {
-        AddNoteFragment fragment = new AddNoteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText editTextTitle = view.findViewById(R.id.editTextTitle);
+        EditText editTextDescription = view.findViewById(R.id.editTextDescription);
+        Button buttonSave = view.findViewById(R.id.buttonSave);
+
+        buttonSave.setOnClickListener(v -> {
+            String title = editTextTitle.getText().toString();
+            String description = editTextDescription.getText().toString();
+
+            if (!title.isEmpty() && !description.isEmpty()) {
+
+                FirstFragment.addNote(new Note(title, description));
+                // Navigate back to FirstFragment
+                NavHostFragment.findNavController(AddNoteFragment.this)
+                        .navigate(R.id.action_addNoteFragment_to_FirstFragment);
+            }
+        });
     }
 }
